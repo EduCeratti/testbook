1109125829612a9e47050df554b209f5066d20d5
ceratti <eduardo.ceratti@corp.terra.com>
2018-09-04 07:58:47 -0300

    atualizando

--- app.py
+++ app.py
@@ -11,8 +11,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker 
 
 from flask import Blueprint
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 Base = declarative_base()
 
@@ -29,38 +31,29 @@ def index():
         db_object = Basedb()
         db_object.insert_row(testplan)
 
-        return render_template('index.html')
-
-    else:
-        return render_template('index.html')
+    
+    return render_template('index.html')
 
 
 
 @app.route('/list', methods=['GET', 'POST'])
 def list():
     if request.method == 'POST':
-        userDetails = request.form
-        name = userDetails['name']
-        email = userDetails['email']
+        testplanDetails = request.form
+        title = testplanDetails['title']
 
-        cur = mysql.connection.cursor()
+        testplan = Testplan()
+        testplan.title = title
+        db_object = Basedb()
+        db_object.insert_row(testplan)
+        testbooks = db_object.select_all_rows(Testplan)     
+        
     else:
-
-        # engine = create_engine('mysql://root:root@localhost/testbook') # connect to server
-        # Base.metadata.create_all(bind=engine)
-        # Session = sessionmaker(bind=engine)
-        # session = Session()
 
         db_object = Basedb()
         testbooks = db_object.select_all_rows(Testplan)
-        print (testbooks)
-        #session.query(Testplan).all()
-
-        # all = []
-        # for testbook in testbooks:
-        #     return (testbook.title)
-
-        return render_template('list_testbook.html', testbooks= testbooks)
+    
+    return render_template('list_testbook.html', testbooks= testbooks)
 
 
 if __name__ == '__main__':
--- models/user.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-
-
-class User(Base):
-    __tablename__ = 'user'
-    name = Column(String(32)
-    email = Column(String(32)
